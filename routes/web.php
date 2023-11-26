9b25621ce4e0faee3c296d67d47c8bc65c53e76e
Bekzod Asus <raximovbekzodbek@mail.ru>
2023-11-26 06:34:14 +0500

    add laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiteController;
 /*
@@ -17,3 +18,15 @@ Route::get('/', [SiteController::class, 'index'])->name('index');
 Route::get('/contact', [SiteController::class, 'contact'])->name('contact');
 Route::get('/posts/{slug}', [SiteController::class, 'postDetail'])->name('postDetail');
 Route::get('/category/{slug}', [SiteController::class, 'categoryPosts'])->name('categoryPosts');
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
